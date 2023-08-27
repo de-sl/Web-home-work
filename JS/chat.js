b10f3362bf5f3801a20113b558417ad6ff3c21f7
de-sl <en060988@gmail.com>
2023-08-27 11:41:23 +0300

    fix js chat

--- JS/chat.js
+++ JS/chat.js
@@ -1,5 +1,5 @@
-var user_name = prompt("Пожалуйста представьтесь")
-
+var user_name = prompt("Пожалуйста, представьтесь")
+document.getElementById("user_nickname").value = user_name
 
 function chat() {
    let us_messege = document.getElementById("user_messege").value
@@ -7,9 +7,39 @@ function chat() {
 
    if (nickname !== "") {
         messege_nickname = nickname + ": " + us_messege
+        document.getElementById('body').value = nickname + ": " + us_messege;
+        
    }
    else {
-       messege_nickname = user_name + ": " + us_messege
+        document.getElementById('body').value = user_name + ": " + us_messege;
+       messege_nickname = user_name + ": " + us_messege     
    }
-    document.getElementById("message1").innerText = messege_nickname
+        
+    document.getElementById("user_messege").value = ' '
+   //if ("messege1" === " "){
+        //  let u_massage1  = document.getElementById("message1").innerText = messege_nickname
+          
+   //}
+    // else (u_massage1 !== " "); {
+     //     document.getElementById("message2").innerText = messege_nickname
+    //      document.getElementById("message1").value = " "
+    // }
+
+    //document.getElementById('body').value = nickname + ": " + us_messege;
+     //document.getElementById("user_messege").value = " "
+    //document.getElementById("message1").innerText = messege_nickname
 }
+
+
+// switch(true) {
+          //case ('message1' == " "):
+                //document.getElementById("message1").innerText = messege_nickname
+     
+         // break
+          
+          //case ('message1' !== " "):
+               //let message1 = document.getElementById("user_messege").value
+                //document.getElementById("message2").innerText = message1
+               // document.getElementById("message1").value = " "
+         // break
+     //}
